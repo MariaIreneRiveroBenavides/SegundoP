a2e1c55ad371095aafa7c07ab8d8853b81eae77c
MariaIreneRiveroBenavides <a2020113350@estudiantes.upsa.edu.bo>
2021-10-13 19:23:41 -0400

    Migrations implementado

---
+++ SegundoParcial/SegundoParcial/Migrations/20211013232313_AddModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SegundoParcial.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Suertes",
+                columns: table => new
+                {
+                    SuerteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Detalle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suertes", x => x.SuerteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Suertes");
+        }
+    }
+}
