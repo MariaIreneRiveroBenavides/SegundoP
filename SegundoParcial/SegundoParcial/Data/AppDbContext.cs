408baf424e09845396ad9c4226d24c43168cd538
MariaIreneRiveroBenavides <a2020113350@estudiantes.upsa.edu.bo>
2021-10-13 19:20:02 -0400

    AppDbContext creado

---
+++ SegundoParcial/SegundoParcial/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SegundoParcial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcial.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Suerte> Suertes { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
