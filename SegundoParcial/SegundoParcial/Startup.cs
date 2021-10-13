71758e65e9134b64076f1c4542f13f7a61e7d249
MariaIreneRiveroBenavides <a2020113350@estudiantes.upsa.edu.bo>
2021-10-13 19:21:07 -0400

    IreneContext agregado a Startup.cs

--- SegundoParcial/SegundoParcial/Startup.cs
+++ SegundoParcial/SegundoParcial/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using SegundoParcial.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +34,14 @@ namespace SegundoParcial
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SegundoParcial", Version = "v1" });
             });
+
+            services.AddDbContext<AppDbContext>
+                (
+                options => options.UseSqlServer(
+                    Configuration.GetConnectionString("IreneContext")
+                    )
+                );
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
