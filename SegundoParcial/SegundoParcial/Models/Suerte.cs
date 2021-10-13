c943f98126703db8b4c061a0ca8c6803a6f2b313
MariaIreneRiveroBenavides <a2020113350@estudiantes.upsa.edu.bo>
2021-10-13 19:14:53 -0400

    Modelo Suerte creado

---
+++ SegundoParcial/SegundoParcial/Models/Suerte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcial.Models
+{
+    public class Suerte
+    {
+        [Key]
+        public int SuerteId { get; set; }
+        [Required(ErrorMessage ="El detalle es requerido")]
+        [Display(Name ="Detalle de la suerte")]
+        public string Detalle { get; set; }
+        [Required]
+        [Url]
+        [Display(Name ="Enlace a imagen de la suerte")]
+        public string Imagen { get; set; }
+    }
+}
